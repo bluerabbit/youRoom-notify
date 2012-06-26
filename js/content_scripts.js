bc5e3b90a2aa1c8aef9d315383e284634cc09a1c
Akira Kusumoto <akirakusumo10@gmail.com>
2012-06-26 22:49:40 +0900

    テキストボックスをコメントし易いように動的にテキストボックスの高さが変わるようにした

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -75,3 +75,14 @@ function have(bookmarkList, url) {
     });
     return result;
 }
+
+// テキストボックスをコメントし易いように動的にテキストボックスの高さが変わるようにする
+$('.entry_content').live('focus', function () {
+    var self = $(this);
+    if (!self.data("has-scroll")) {
+        self.data("has-scroll", true);
+        self.scroll(function () {
+            self.css('height', (parseInt(self.css('height').slice(0,-2)) + 10) + 'px');
+        });
+    }
+});
